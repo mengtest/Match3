65be279452af05d716e9a30d4c3f2600e2d25592
Tomasz Swierkot <tomek.swierkot@gmail.com>
2017-02-09 09:19:28 +0100

    Added Camera resizer script. Added Score text to UI.

---
+++ Assets/Scripts/CameraResizer.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class CameraResizer : MonoBehaviour
+{
+    public Bounds targetBounds;
+    Resolution res;
+
+    void Start()
+    {
+        ResizeCamera();
+        res = Screen.currentResolution;
+    }
+
+    void LateUpdate()
+    {
+        if (res.width != Screen.currentResolution.width || res.height != Screen.currentResolution.height)
+        {
+            ResizeCamera();
+            res = Screen.currentResolution;
+        }
+    }
+
+    void ResizeCamera()
+    {
+        float screenRatio = (float)Screen.width / (float)Screen.height;
+        float targetRatio = targetBounds.size.x / targetBounds.size.y;
+
+        if (screenRatio >= targetRatio)
+        {
+            Camera.main.orthographicSize = targetBounds.size.y / 2;
+        }
+        else
+        {
+            float differenceInSize = targetRatio / screenRatio;
+            Camera.main.orthographicSize = targetBounds.size.y / 2 * differenceInSize;
+        }
+
+        transform.position = new Vector3(targetBounds.center.x, targetBounds.center.y, -15f);
+    }
+
+}
